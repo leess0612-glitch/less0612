78b45b8b88e67b493933b22ae2b916439e33097f
leess0612-glitch <leess0612@gmail.com>
2026-02-25 12:06:27 +0900

    auto: 2026. 2. 25. 오후 12:06:27

--- download_model.py
+++ download_model.py
@@ -1,22 +1,31 @@
 import os
 import urllib.request
 
-url   = 'https://github.com/danielgatis/rembg/releases/download/v0.0.0/u2netp.onnx'
-dest  = os.path.join(os.path.expanduser('~'), '.u2net', 'u2netp.onnx')
+models = [
+    ('u2net.onnx',  'https://github.com/danielgatis/rembg/releases/download/v0.0.0/u2net.onnx',  '176MB - 고성능'),
+    ('u2netp.onnx', 'https://github.com/danielgatis/rembg/releases/download/v0.0.0/u2netp.onnx', '4.7MB - 경량'),
+]
 
-os.makedirs(os.path.dirname(dest), exist_ok=True)
+dest_dir = os.path.join(os.path.expanduser('~'), '.u2net')
+os.makedirs(dest_dir, exist_ok=True)
 
-if os.path.exists(dest):
-    print(f'이미 존재함: {dest}')
-else:
-    print(f'다운로드 시작: {url}')
-    print('잠시 기다려주세요...')
+for fname, url, desc in models:
+    dest = os.path.join(dest_dir, fname)
+    if os.path.exists(dest):
+        print(f'[이미 있음] {fname} ({desc})')
+        continue
+
+    print(f'\n[다운로드] {fname} ({desc})')
+    print(f'URL: {url}')
 
     def progress(count, block, total):
         pct = min(100, int(count * block / total * 100))
-        print(f'\r진행: {pct}%', end='', flush=True)
+        done = int(pct / 2)
+        bar = '#' * done + '-' * (50 - done)
+        print(f'\r[{bar}] {pct}%', end='', flush=True)
 
     urllib.request.urlretrieve(url, dest, reporthook=progress)
     print(f'\n완료: {dest}')
 
+print('\n--- 모든 모델 준비 완료 ---')
 input('\n아무 키나 누르면 닫힙니다...')
