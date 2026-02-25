c5c9f1533f63c84e97091ecbbc3aa43ab4ce17d7
leess0612-glitch <leess0612@gmail.com>
2026-02-25 09:45:11 +0900

    auto: 2026. 2. 25. 오전 9:45:11

--- 실행_배경제거.bat
+++ 실행_배경제거.bat
@@ -1,4 +1,5 @@
 @echo off
-py -m pip install PyQt5 Pillow onnxruntime "rembg[cpu]"
+py -m pip uninstall -y onnxruntime onnxruntime-gpu onnxruntime-silicon
+py -m pip install PyQt5 Pillow "rembg[cpu]"
 py "%~dp0bg_remove.py"
 pause
